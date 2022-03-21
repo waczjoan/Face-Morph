0e4af521c91f21091c3a9fd094e5c74413e7eaf4
Waczjoan <asia.wacz@gmail.com>
2022-03-21 22:56:16 +0100

    detect face landmarks -- 68 and 468 points

--- src/landmarks.py
+++ src/landmarks.py
@@ -1,3 +1,7 @@
+import mediapipe as mp
+import numpy as np
+
+
 def get_info_68_points():
     landmarks_68_points = {
         'CONTOURS' : list(range(17)),
@@ -14,6 +18,8 @@ def get_info_68_points():
     LEFT_FACE_SIDE, REIGHT_FACE_SIDE = left_face_side_68_points(landmarks_68_points)
     landmarks_68_points['LEFT_FACE_SIDE'] = LEFT_FACE_SIDE
     landmarks_68_points['REIGHT_FACE_SIDE'] = REIGHT_FACE_SIDE
+    symmetry = {}
+    return landmarks_68_points, symmetry
     
 
 def get_info_106_points():
@@ -35,6 +41,26 @@ def get_info_106_points():
     landmarks_106_points['REIGHT_FACE_SIDE'] = REIGHT_FACE_SIDE
     symmetry = landmarks_106_symmetry(landmarks_106_points)
     return landmarks_106_points, symmetry
+
+
+def get_info_468_points():
+    landmarks_468_points = {
+        'CONTOURS' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_FACE_OVAL)).flatten())), 
+        'LEFT_EYEBROWN' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_LEFT_EYEBROW)).flatten())),
+        'REIGHT_EYEBROWN' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_RIGHT_EYEBROW)).flatten())),
+        'NOSE_POINTS' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_LIPS)).flatten())), #####
+        'NOSE_LINE_POINTS' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_LIPS )).flatten())), #####
+        'LEFT_EYE' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_LEFT_EYE)).flatten())), 
+        'REIGHT_EYE' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_RIGHT_EYE)).flatten())),  
+        'UPPER_LIP' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_LIPS)).flatten())), #####
+        'BOTTOM_LIP' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_LIPS)).flatten())), #####
+        'TEETH' : list(set(np.array(list(mp.solutions.face_mesh.FACEMESH_LIPS)).flatten())), ########
+    }
+    #LEFT_FACE_SIDE, REIGHT_FACE_SIDE = left_face_side_106_points(landmarks_106_points)
+    #landmarks_468_points['LEFT_FACE_SIDE'] = LEFT_FACE_SIDE
+    #landmarks_468_points['REIGHT_FACE_SIDE'] = REIGHT_FACE_SIDE
+    symmetry = {}
+    return landmarks_468_points, symmetry
                          
 
 def add_to_sym(symmetry, temp_reight, temp_left, reverse = True):
@@ -62,6 +88,25 @@ def left_face_side_106_points(landmarks):
         landmarks['UPPER_LIP'][int(len(landmarks['UPPER_LIP'])*1/2):] + \
         landmarks['BOTTOM_LIP'][:int(len(landmarks['BOTTOM_LIP'])*1/2)+1] + \
         landmarks['REIGHT_EYE'] + [landmarks['EYE'][1]] + landmarks['NOSE_LINE_POINTS'] + \
+        landmarks['NOSE_POINTS'][int(len(landmarks['NOSE_POINTS'])/2)+2:]
+    return LEFT_FACE_SIDE, REIGHT_FACE_SIDE
+
+
+def left_face_side_68_points(landmarks): 
+    LEFT_FACE_SIDE =  landmarks['LEFT_EYEBROWN'] +\
+        landmarks['CONTOURS'][:int(len(landmarks['CONTOURS'])/2)+1] +\
+        landmarks['TEETH'][int(len(landmarks['TEETH'])*3/4):] + \
+        landmarks['TEETH'][:int(len(landmarks['TEETH'])*1/4 + 1)] +\
+        landmarks['UPPER_LIP'][:int(len(landmarks['UPPER_LIP'])*1/2 + 1)] + \
+        landmarks['BOTTOM_LIP'][int(len(landmarks['BOTTOM_LIP'])*1/2):] + \
+        landmarks['LEFT_EYE'] +  landmarks['NOSE_POINTS'][:int(len(landmarks['NOSE_POINTS'])/2)+3]
+    
+    REIGHT_FACE_SIDE =  landmarks['REIGHT_EYEBROWN'] +\
+        landmarks['CONTOURS'][int(len(landmarks['CONTOURS'])/2):] +\
+        landmarks['TEETH'][int(len(landmarks['TEETH'])*1/4):int(len(landmarks['TEETH'])*3/4)+1] + \
+        landmarks['UPPER_LIP'][int(len(landmarks['UPPER_LIP'])*1/2):] + \
+        landmarks['BOTTOM_LIP'][:int(len(landmarks['BOTTOM_LIP'])*1/2)+1] + \
+        landmarks['REIGHT_EYE'] + landmarks['NOSE_LINE_POINTS'] + \
         landmarks['NOSE_POINTS'][int(len(landmarks['NOSE_POINTS'])/2)+2:]
     return LEFT_FACE_SIDE, REIGHT_FACE_SIDE
 
@@ -105,6 +150,10 @@ class Landmarks():
         if len(self.landmarks_array) == 106:
             self.points_info, self.symmetry = get_info_106_points()
             self.symmetry_line = [i for i in self.symmetry.keys() if i == self.symmetry[i]]
+        elif len(self.landmarks_array) == 68:
+            self.points_info, self.symmetry = get_info_68_points()
+        elif len(self.landmarks_array) == 468:
+            self.points_info, self.symmetry = get_info_468_points()
 
     def __call__(self):
         return self.landmarks_array
--- src/visualization.py
+++ src/visualization.py
@@ -11,20 +11,20 @@ def plot_img(image, ax = plt, title = None, alpha = 1):
             ax.set_title(title)
             
             
-def plot_landmarks_color(landmarks):
+def plot_landmarks_color(landmarks, ax = plt):
         r = landmarks.landmarks_array
         landmarks_point_info = landmarks.points_info
-        plt.plot(r[landmarks_point_info['CONTOURS'],0], r[landmarks_point_info['CONTOURS'],1], 'b.')
-        plt.plot(r[landmarks_point_info['LEFT_EYEBROWN'],0], r[landmarks_point_info['LEFT_EYEBROWN'],1], 'r.')
-        plt.plot(r[landmarks_point_info['REIGHT_EYEBROWN'],0], r[landmarks_point_info['REIGHT_EYEBROWN'],1], 'g.')
-        plt.plot(r[landmarks_point_info['NOSE_POINTS'],0], r[landmarks_point_info['NOSE_POINTS'],1], 'k.')
-        plt.plot(r[landmarks_point_info['LEFT_EYE'],0], r[landmarks_point_info['LEFT_EYE'],1], '.', color =  'white')
-        plt.plot(r[landmarks_point_info['REIGHT_EYE'],0], r[landmarks_point_info['REIGHT_EYE'],1], '.', color = 'purple')
-        plt.plot(r[landmarks_point_info['UPPER_LIP'],0], r[landmarks_point_info['UPPER_LIP'],1], '.', color =  'pink')
-        plt.plot(r[landmarks_point_info['BOTTOM_LIP'],0], r[landmarks_point_info['BOTTOM_LIP'],1], '.', color =  'orange')
-        plt.plot(r[landmarks_point_info['TEETH'],0], r[landmarks_point_info['TEETH'],1], '.', color =  'yellow')
+        ax.plot(r[landmarks_point_info['CONTOURS'],0], r[landmarks_point_info['CONTOURS'],1], 'b.')
+        ax.plot(r[landmarks_point_info['LEFT_EYEBROWN'],0], r[landmarks_point_info['LEFT_EYEBROWN'],1], 'r.')
+        ax.plot(r[landmarks_point_info['REIGHT_EYEBROWN'],0], r[landmarks_point_info['REIGHT_EYEBROWN'],1], 'g.')
+        ax.plot(r[landmarks_point_info['NOSE_POINTS'],0], r[landmarks_point_info['NOSE_POINTS'],1], 'k.')
+        ax.plot(r[landmarks_point_info['LEFT_EYE'],0], r[landmarks_point_info['LEFT_EYE'],1], '.', color =  'white')
+        ax.plot(r[landmarks_point_info['REIGHT_EYE'],0], r[landmarks_point_info['REIGHT_EYE'],1], '.', color = 'purple')
+        ax.plot(r[landmarks_point_info['UPPER_LIP'],0], r[landmarks_point_info['UPPER_LIP'],1], '.', color =  'pink')
+        ax.plot(r[landmarks_point_info['BOTTOM_LIP'],0], r[landmarks_point_info['BOTTOM_LIP'],1], '.', color =  'orange')
+        ax.plot(r[landmarks_point_info['TEETH'],0], r[landmarks_point_info['TEETH'],1], '.', color =  'yellow')
         if 'EYE' in landmarks_point_info.keys():
-            plt.plot(r[landmarks_point_info['EYE'],0], r[landmarks_point_info['EYE'],1], '.', color =  'tab:blue')
+            ax.plot(r[landmarks_point_info['EYE'],0], r[landmarks_point_info['EYE'],1], '.', color =  'tab:blue')
             
             
 def plot_corr(x, y, cmap = plt.get_cmap('RdBu'), norm = plt.Normalize(vmin=-.5, vmax=.5),**kwds):
